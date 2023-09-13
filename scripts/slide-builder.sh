c16fea1d965c71dbb7d6edc377343f5ff0be2f30
Teruki TADA <tadateruki.public@gmail.com>
2023-09-13 20:17:08 +0900

    [script] bun -> npm

--- scripts/slide-builder.sh
+++ scripts/slide-builder.sh
@@ -1,2 +1,2 @@
 
-bunx @marp-team/marp-cli@latest $1/slide.md --output $1/index.html --theme ./themes/perutheme0.css
+npx @marp-team/marp-cli@latest $1/slide.md --output $1/index.html --theme ./themes/perutheme0.css

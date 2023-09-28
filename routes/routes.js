135008d7c6b21182edacba6522a8d512f51e7d37
Teruki TADA <tadateruki.public@gmail.com>
2023-09-28 15:51:47 +0000

    [build] 2023/09/28

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
 const SLIDE_ROUTES = [
+["slides/2023/FUNAI/9_12/", {"source":"index.html","date":"2023/9/12","title":"FUNAI輪読会資料: 誤差逆伝播法","description":"ゼロから作るDeepLearning 5章 誤差逆伝播法 5.1-5.3の資料です。"}],
+["slides/2023/FUNAI/9_25/", {"source":"index.html","date":"2002/8/14","title":"タイトル","description":"詳細"}],
 ["slides/2023/LT/函館市電LT/content/", {"source":"index.html","date":"2023/9/18","title":"行かなきゃ人生終了！道南の素晴らしいロードサイド3選","description":"函館市電LTの発表資料です。"}],
 ["slides/2023/LT/函館市電LT/opening/", {"source":"index.html","date":"2023/9/18","title":"函館市電LT オープニング","description":"函館市電LTの開会用の資料です。"}],
 ["slides/2023/LT/ゆるい勉強会in旭川/yuruasa/", {"source":"index.html","date":"2023/9/10","title":"MIERUNEインターンに行ってきた","description":"ゆるい勉強会(旭川)での発表内容です。"}],
-["slides/2023/新雪プログラム/ブースト会議/", {"source":"index.html","date":"2023/9/17","title":"新雪プログラム ブースト会議","description":"新雪プログラムのプロジェクト「ゲーム・映像表現の可能性を広げる地形生成ライブラリの開発」の説明資料です。"}],
-["slides/2023/FUNAI/9_12/", {"source":"index.html","date":"2023/9/12","title":"FUNAI輪読会資料: 誤差逆伝播法","description":"ゼロから作るDeepLearning 5章 誤差逆伝播法 5.1-5.3の資料です。"}],
-["slides/2023/FUNAI/9_25/", {"source":"index.html","date":"2002/8/14","title":"タイトル","description":"詳細"}],
 ["slides/2023/Others/mierune/", {"source":"index.html","date":"2023/9/8","title":"MIERUNEインターンシップ 成果発表","description":"MIERUNE Inc. インターンシップでの成果発表の資料です。"}],
+["slides/2023/新雪プログラム/ブースト会議/", {"source":"index.html","date":"2023/9/17","title":"新雪プログラム ブースト会議","description":"新雪プログラムのプロジェクト「ゲーム・映像表現の可能性を広げる地形生成ライブラリの開発」の説明資料です。"}],
 ];

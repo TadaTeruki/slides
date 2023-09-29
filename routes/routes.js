ef944ae567799d2e24e06cbfafaf2f87e7e6a7f0
Teruki TADA <tadateruki.public@gmail.com>
2023-09-29 03:37:54 +0000

    [build] 2023/09/29

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const SLIDE_ROUTES = [
 ["slides/2023/FUNAI/9_12/", {"source":"index.html","date":"2023/9/12","title":"FUNAI輪読会資料: 誤差逆伝播法","description":"ゼロから作るDeepLearning 5章 誤差逆伝播法 5.1-5.3の資料です。"}],
 ["slides/2023/FUNAI/9_25/", {"source":"index.html","date":"2002/8/14","title":"タイトル","description":"詳細"}],
+["slides/2023/LT/TOGATTA/", {"source":"index.html","date":"2023-9-30","title":"tinyWMをちゃんと読む","description":"tinyWMの処理の流れを理解し、X11でのウインドウマネージャ開発の感覚を掴む"}],
 ["slides/2023/LT/函館市電LT/content/", {"source":"index.html","date":"2023/9/18","title":"行かなきゃ人生終了！道南の素晴らしいロードサイド3選","description":"函館市電LTの発表資料です。"}],
 ["slides/2023/LT/函館市電LT/opening/", {"source":"index.html","date":"2023/9/18","title":"函館市電LT オープニング","description":"函館市電LTの開会用の資料です。"}],
 ["slides/2023/LT/ゆるい勉強会in旭川/yuruasa/", {"source":"index.html","date":"2023/9/10","title":"MIERUNEインターンに行ってきた","description":"ゆるい勉強会(旭川)での発表内容です。"}],

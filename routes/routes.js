eb084aa03dd25ac045b7e62956c9c2aefa94469e
Teruki TADA <tadateruki.public@gmail.com>
2023-10-07 13:06:08 +0000

    build: 2023/10/07

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const SLIDE_ROUTES = [
 ["slides/2023/LT/TOGATTA/vol4/", {"source":"index.html","date":"2023/9/30","title":"読むWM tinyWM をちゃんと読む","description":"tinyWMの処理の流れを理解し、X11でのウインドウマネージャ開発の感覚を掴む"}],
 ["slides/2023/LT/函館市電LT/content/", {"source":"index.html","date":"2023/9/18","title":"行かなきゃ人生終了！道南の素晴らしいロードサイド3選","description":"函館市電LTの発表資料です。"}],
 ["slides/2023/LT/函館市電LT/opening/", {"source":"index.html","date":"2023/9/18","title":"函館市電LT オープニング","description":"函館市電LTの開会用の資料です。"}],
+["slides/2023/LT/未来祭LT/", {"source":"index.html","date":"2023/10/07","title":"読むWM tinyWM をちゃんと読む","description":"tinyWMの処理の流れを理解し、X11でのウインドウマネージャ開発の感覚を掴む"}],
 ["slides/2023/LT/ゆるい勉強会in旭川/yuruasa/", {"source":"index.html","date":"2023/9/10","title":"MIERUNEインターンに行ってきた","description":"ゆるい勉強会(旭川)での発表内容です。"}],
 ["slides/2023/Others/mierune/", {"source":"index.html","date":"2023/9/8","title":"MIERUNEインターンシップ 成果発表","description":"MIERUNE Inc. インターンシップでの成果発表の資料です。"}],
 ["slides/2023/新雪プログラム/ブースト会議/", {"source":"index.html","date":"2023/9/17","title":"新雪プログラム ブースト会議","description":"新雪プログラムのプロジェクト「ゲーム・映像表現の可能性を広げる地形生成ライブラリの開発」の説明資料です。"}],
